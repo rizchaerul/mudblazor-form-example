64c782e1bab3412cebeb0ba0554009100595ab9a
Chaerul Rizky <riz.chaerul@gmail.com>
2024-05-29 00:24:32 +0700

    add autocomplete example

--- Application.Client/Pages/FormExample.razor.cs
+++ Application.Client/Pages/FormExample.razor.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using MudBlazor;
@@ -47,18 +48,12 @@ public partial class FormExample : ComponentBase
 
         if (string.IsNullOrWhiteSpace(Form.FullName))
         {
-            ValidationMessageStore.Add(
-                fieldIdentifier: EditContext.Field(nameof(Form.FullName)),
-                message: "FullName cannot be empty."
-            );
+            ValidationMessageStore.Add(EditContext.Field(nameof(Form.FullName)), "FullName cannot be empty.");
         }
 
         if (Form.DateOfBirth <= new DateTime(2000, 1, 1))
         {
-            ValidationMessageStore.Add(
-                fieldIdentifier: EditContext.Field(nameof(Form.DateOfBirth)),
-                message: "You're too old."
-            );
+            ValidationMessageStore.Add(EditContext.Field(nameof(Form.DateOfBirth)), "You're too old.");
         }
 
         var isValid = EditContext.Validate();
@@ -82,6 +77,36 @@ public partial class FormExample : ComponentBase
 
         IsLoading = false;
     }
+
+    async Task<IEnumerable<AutoCompleteModel?>> GetSongs(string searchText)
+    {
+        if (string.IsNullOrWhiteSpace(searchText))
+            searchText = "Hey Jude";
+
+        try
+        {
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri($"https://itunes.apple.com/search?term={searchText}&media=music&entity=musicTrack")
+            };
+
+            var response = await client.SendAsync(new HttpRequestMessage { Method = HttpMethod.Get, });
+            // throw error jika bukan 200
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<ITunesResponse>();
+
+            return result!.Results.Select(x => new AutoCompleteModel
+            {
+                Id = x.TrackId.ToString(),
+                Description = x.ArtistName + " - " + x.TrackName
+            });
+        }
+        catch
+        {
+            return new List<AutoCompleteModel?>();
+        }
+    }
 }
 
 public class StudentFormModel
@@ -92,4 +117,25 @@ public class StudentFormModel
     public string Address { get; set; } = string.Empty;
 
     public DateTime? DateOfBirth { get; set; }
+
+    public AutoCompleteModel? FavoriteSong { get; set; }
+}
+
+public class AutoCompleteModel
+{
+    public string Id { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
+
+public class ITunesResponse
+{
+    public int ResultCount { get; set; }
+    public List<ITunesResponseDetail> Results { get; set; } = new();
+}
+
+public class ITunesResponseDetail
+{
+    public string ArtistName { get; set; } = string.Empty;
+    public int TrackId { get; set; }
+    public string TrackName { get; set; } = string.Empty;
 }
